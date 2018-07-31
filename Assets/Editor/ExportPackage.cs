60cc324c5b21d20626f78b7a1b03afd714f47ed3
hiyorin <ponkotsu.hiyorin@gmail.com>
2018-07-31 15:26:36 +0900

    [add] include readme, license into unitypackage

--- Assets/Editor/ExportPackage.cs
+++ Assets/Editor/ExportPackage.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class ExportPackage
 {
@@ -6,9 +8,23 @@ public class ExportPackage
         "Assets/Plugins/SocialGameTemplete",
     };
 
+    private const string ReadMe = "README.md";
+    private const string License = "LICENSE";
+
     [MenuItem("Assets/Export SocialGameTemplete")]
     private static void Export()
     {
+        string readmePath = Path.Combine(Application.dataPath, "Plugins/SocialGameTemplete", ReadMe);
+        string licensePath = Path.Combine(Application.dataPath, "Plugins/SocialGameTemplete", License);
+        File.Copy(Path.Combine(Application.dataPath, "..", ReadMe), readmePath);
+        File.Copy(Path.Combine(Application.dataPath, "..", License), licensePath);
+        AssetDatabase.Refresh();
+
         AssetDatabase.ExportPackage(Paths, "SocialGameTemplete.unitypackage", ExportPackageOptions.Recurse);
+        Debug.Log("Export complete!");
+
+        File.Delete(readmePath);
+        File.Delete(licensePath);
+        AssetDatabase.Refresh();
     }
 }
