547f8ba34eac65394105f19d727e57f9cb50bbec
hiyorin <ponkotsu.hiyorin@gmail.com>
2019-01-23 23:17:21 +0900

    [modify] grpc output directory

--- tools/grpc/build_client.sh
+++ tools/grpc/build_client.sh
@@ -1,8 +1,8 @@
 # 出力先を作成
-mkdir -p ../Assets/Scripts/Data/Entity
+mkdir -p ../../Assets/Scripts/Data/Entity
 
 # コード生成
-protoc -I protos --csharp_out ../Assets/Scripts/Data/Entity --grpc_out ../Assets/Scripts/Data/Entity protos/*.proto --plugin=protoc-gen-grpc=Grpc.Tools/tools/macosx_x64/grpc_csharp_plugin
+protoc -I protos --csharp_out ../../Assets/Scripts/Data/Entity --grpc_out ../../Assets/Scripts/Data/Entity protos/*.proto --plugin=protoc-gen-grpc=Grpc.Tools/tools/macosx_x64/grpc_csharp_plugin
 
 # 確認
-ls ../Assets/Data/Entity
+ls ../../Assets/Data/Entity
