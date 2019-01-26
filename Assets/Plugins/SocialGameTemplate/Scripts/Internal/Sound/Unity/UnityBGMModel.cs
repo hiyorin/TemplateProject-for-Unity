f50fb4ba7bdc65d167b3127fd43bd3b293b90e77
hiyorin <ponkotsu.hiyorin@gmail.com>
2019-01-27 00:57:57 +0900

    [add] asset bundle function of unity sound

--- Assets/Plugins/SocialGameTemplate/Scripts/Internal/Sound/Unity/UnityBGMModel.cs
+++ Assets/Plugins/SocialGameTemplate/Scripts/Internal/Sound/Unity/UnityBGMModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using SocialGame.Sound;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityExtensions;
 using Zenject;
 using UniRx;
+using UniRx.Async;
 
 namespace SocialGame.Internal.Sound.Unity
 {
@@ -16,50 +19,36 @@ namespace SocialGame.Internal.Sound.Unity
 
         [Inject] private UnityBGMSettings _settings = null;
 
-        private ReactiveCollection<AudioSource> _audioSources = new ReactiveCollection<AudioSource>();
+        private readonly ReactiveCollection<AudioSource> _sources = new ReactiveCollection<AudioSource>();
 
+        private readonly Dictionary<string, AudioClip> _clips = new Dictionary<string,AudioClip>();
+        
         private readonly CompositeDisposable _disposable = new CompositeDisposable();
 
         void IInitializable.Initialize()
         {
             if (_settings.Group == null)
                 return;
-            
-            _intent.OnPlayAsObservable()
-                .SelectMany(bgm => {
-                    var afterSource = _audioSources.FirstOrDefault(x => !x.enabled);
-                    if (afterSource == null)
-                        return Observable.ReturnUnit();
 
-                    var clip = _settings.Clips.ElementAt((int)bgm);
-                    var beforeSource = _audioSources.FirstOrDefault(x => x.enabled);
-                    afterSource.enabled = true;
-                    afterSource.PlayWithFadeIn(clip, 1.0f, _settings.FadeInDuration);
-                    if (beforeSource == null)
-                        return Observable.ReturnUnit();
-                    else
-                        return beforeSource
-                            .FadeOutAsCoroutine(_settings.FadeOutDuration)
-                            .ToObservable()
-                            .Do(_ => {
-                                beforeSource.Stop();
-                                beforeSource.enabled = false;
-                            });
-                })
-                .Subscribe()
+            foreach (BGM bgm in Enum.GetValues(typeof(BGM)))
+            {
+                var clip = _settings.Clips.ElementAt((int)bgm);
+                _clips.Add(bgm.ToString(), clip);
+            }
+            
+            Observable.Merge(
+                    _intent.OnPlayAsObservable().Select(x => x.ToString()),
+                    _intent.OnPlayForNameAsObservable())
+                .Subscribe(x => Play(x).GetAwaiter())
                 .AddTo(_disposable);
 
-            _intent.OnPlayForNameAsObservable()
-                .Subscribe(_ => Debug.unityLogger.LogWarning(GetType().Name, "Not supported"))
-                .AddTo(_disposable);
-            
             _intent.OnStopAsObservable()
-                .SelectMany(_ => _audioSources
+                .SelectMany(_ => _sources
                     .Select(x => x.FadeOutAsCoroutine(_settings.FadeOutDuration).ToObservable())
                     .WhenAll()
                     .First())
                 .Subscribe(_ => {
-                    _audioSources.ForEach(x => {
+                    _sources.ForEach(x => {
                         x.Stop();
                         x.enabled = false;
                     });
@@ -76,13 +65,54 @@ namespace SocialGame.Internal.Sound.Unity
                 var audioSource = new GameObject($"BGM_{i:000}").AddComponent<AudioSource>();
                 audioSource.outputAudioMixerGroup = _settings.Group;
                 audioSource.enabled = false;
-                _audioSources.Add(audioSource);
+                _sources.Add(audioSource);
             }
         }
 
         void IDisposable.Dispose()
         {
             _disposable.Dispose();
+        }
+
+        private async UniTask Play(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Debug.unityLogger.LogError(GetType().Name, $"name is null or empty");
+                return;
+            }
+            
+            var afterSource = _sources.FirstOrDefault(x => !x.enabled);
+            if (afterSource == null)
+            {
+                Debug.unityLogger.LogWarning(GetType().Name, $"could not play {name}, because of the number of playback.");
+                return;
+            }
+
+            AudioClip clip;
+            if (!_clips.TryGetValue(name, out clip))
+            {
+                var operation = Addressables.LoadAsset<AudioClip>(name);
+                await operation.ToUniTask();
+                clip = operation.Result;
+                _clips.Add(name, clip);
+            }
+
+            if (clip == null)
+            {
+                Debug.unityLogger.LogError(GetType().Name, $"{name} is not found.");
+                return;
+            }
+            
+            var beforeSource = _sources.FirstOrDefault(x => x.enabled);
+            afterSource.enabled = true;
+            afterSource.PlayWithFadeIn(clip, 1.0f, _settings.FadeInDuration);
+
+            if (beforeSource == null)
+                return;
+            await beforeSource.FadeOutAsCoroutine(_settings.FadeOutDuration).ToUniTask();
+            beforeSource.Stop();
+            beforeSource.enabled = false;
         }
 
         #region ISoundModel implementation
@@ -93,7 +123,7 @@ namespace SocialGame.Internal.Sound.Unity
         
         IObservable<Transform> ISoundModel.OnAddObjectAsObservable()
         {
-            return _audioSources
+            return _sources
                 .ObserveAdd()
                 .Select(x => x.Value.transform);
         }
--- Assets/Plugins/SocialGameTemplate/Scripts/Internal/Sound/Unity/UnitySEModel.cs
+++ Assets/Plugins/SocialGameTemplate/Scripts/Internal/Sound/Unity/UnitySEModel.cs
@@ -3,9 +3,11 @@ using System.Linq;
 using System.Collections.Generic;
 using SocialGame.Sound;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityExtensions;
 using Zenject;
 using UniRx;
+using UniRx.Async;
 
 namespace SocialGame.Internal.Sound.Unity
 {
@@ -17,26 +19,31 @@ namespace SocialGame.Internal.Sound.Unity
 
         [Inject] private UnitySESettings _settings = null;
 
-        private readonly ReactiveCollection<AudioSource> _audioSources = new ReactiveCollection<AudioSource>();
+        private readonly ReactiveCollection<AudioSource> _sources = new ReactiveCollection<AudioSource>();
 
         private readonly Queue<AudioClip> _playQueue = new Queue<AudioClip>();
 
+        private readonly Dictionary<string, AudioClip> _clips = new Dictionary<string, AudioClip>();
+        
         private readonly CompositeDisposable _disposable = new CompositeDisposable();
 
         void IInitializable.Initialize()
         {
             if (_settings.Group == null)
                 return;
+
+            foreach (SE se in Enum.GetValues(typeof(SE)))
+            {
+                var clip = _settings.Clips.ElementAt((int)se);
+                _clips.Add(se.ToString(), clip);
+            }
             
-            _intent.OnPlayAsObservable()
-                .Select(x => _settings.Clips.ElementAt((int)x))
-                .Subscribe(x => _playQueue.Enqueue(x))
+            Observable.Merge(
+                    _intent.OnPlayAsObservable().Select(x => x.ToString()),
+                    _intent.OnPlayForNameAsObservable())
+                .Subscribe(x => Play(x).GetAwaiter())
                 .AddTo(_disposable);
 
-            _intent.OnPlayForNameAsObservable()
-                .Subscribe(_ => Debug.unityLogger.LogWarning(GetType().Name, "not supported"))
-                .AddTo(_disposable);
-            
             _volumeIntent.OnSEVolumeAsObservable()
                 .Select(x => Mathf.Lerp(-80.0F, 0.0F, Mathf.Clamp01(x)))
                 .Subscribe(x => _settings.Group.audioMixer.SetFloat(_settings.VolumeExposedParameter, x))
@@ -48,9 +55,9 @@ namespace SocialGame.Internal.Sound.Unity
                 .Select(_ => _playQueue.Distinct())
                 .Subscribe(x => {
                     x.ForEach(clip => {
-                        var audioSource = _audioSources[playIndex];
+                        var audioSource = _sources[playIndex];
                         audioSource.PlayOneShot(clip);
-                        if (++playIndex >= _audioSources.Count)
+                        if (++playIndex >= _sources.Count)
                             playIndex = 0;
                     });
                     _playQueue.Clear();
@@ -61,7 +68,7 @@ namespace SocialGame.Internal.Sound.Unity
             {
                 var audioSource = new GameObject($"SE_{i:000}").AddComponent<AudioSource>();
                 audioSource.outputAudioMixerGroup = _settings.Group;
-                _audioSources.Add(audioSource);
+                _sources.Add(audioSource);
             }
         }
 
@@ -70,6 +77,32 @@ namespace SocialGame.Internal.Sound.Unity
             _disposable.Dispose();
         }
 
+        private async UniTask Play(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                Debug.unityLogger.LogError(GetType().Name, $"name is null or empty.");
+                return;
+            }
+
+            AudioClip clip;
+            if (!_clips.TryGetValue(name, out clip))
+            {
+                var operation = Addressables.LoadAsset<AudioClip>(name);
+                await operation.ToUniTask();
+                clip = operation.Result;
+                _clips.Add(name, clip);
+            }
+
+            if (clip == null)
+            {
+                Debug.unityLogger.LogError(GetType().Name, $"{name} is not found.");
+                return;
+            }
+            
+            _playQueue.Enqueue(clip);
+        }
+        
         #region ISoundModel implementation
         IObservable<Unit> ISoundModel.OnInitializedAsObservable()
         {
@@ -78,7 +111,7 @@ namespace SocialGame.Internal.Sound.Unity
         
         IObservable<Transform> ISoundModel.OnAddObjectAsObservable()
         {
-            return _audioSources
+            return _sources
                 .ObserveAdd()
                 .Select(x => x.Value.transform);
         }
