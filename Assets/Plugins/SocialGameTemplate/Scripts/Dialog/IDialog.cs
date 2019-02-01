c06e93d6f074a6e4ace8b2c2e54118ba623af4f7
hiyorin <ponkotsu.hiyorin@gmail.com>
2019-02-02 00:28:16 +0900

    [update] dialog readme

--- Assets/Plugins/SocialGameTemplate/Scripts/Dialog/IDialog.cs
+++ Assets/Plugins/SocialGameTemplate/Scripts/Dialog/IDialog.cs
@@ -1,21 +1,48 @@
 ﻿using System;
-using UniRx;
 using UniRx.Async;
 
 namespace SocialGame.Dialog
 {
     public interface IDialog
     {
+        /// <summary>
+        /// Open interaction
+        /// </summary>
+        /// <param name="defaultDuration"></param>
+        /// <returns></returns>
         UniTask OnOpen(float defaultDuration);
 
+        /// <summary>
+        /// Close interaction
+        /// </summary>
+        /// <param name="defaultDuration"></param>
+        /// <returns></returns>
         UniTask OnClose(float defaultDuration);
         
+        /// <summary>
+        /// Received open param
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
         UniTask OnStart(object param);
 
+        /// <summary>
+        /// Received resume param
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
         UniTask OnResume(object param);
 
+        /// <summary>
+        /// Open next dialog.
+        /// </summary>
+        /// <returns></returns>
         IObservable<RequestDialog> OnNextAsObservable();
 
+        /// <summary>
+        /// Open prev dialog.
+        /// </summary>
+        /// <returns></returns>
         IObservable<object> OnPreviousAsObservable();
     }
 }
--- Documentation/Dialog.md
+++ Documentation/Dialog.md
@@ -1,6 +1,7 @@
 # Dialog
 Dialog Module.  
 
+
 ## DialogSettings
 Resources/DialogSettings.asset
 
@@ -9,3 +10,43 @@ Resources/DialogSettings.asset
 ```cs
 using SocialGame.Dialog;
 ```
+
+### Example
+Assets/Plugins/SocialGameTemplate/Examples/Dialog/DialogExample.unity
+
+### Example: Usage
+```cs
+[Inject] private IDialogController _dialogController;
+private void Example() {
+  _dialogController.Open<string>(DialogType.Sample, UnityEngine.Random.Range(0, 100).ToString()))
+    .Subscribe(x => Debug.Log(x))
+    .AddTo(*);
+}
+```
+
+### Example: implementation
+```cs
+public class DialogExample : MonoBehaviour, IDialog {
+  public UniTask OnOpen(float defaultDuration) {
+
+  }
+  public UniTask OnClose(float defaultDuration) {
+
+  }
+  public UniTask OnStart(object param) {
+
+  }
+  public UniTask OnResume(object param) {
+
+  }
+  public IObservable<RequestDialog> OnNextAsObservable() {
+
+  }
+  public IObservable<object> OnPreviousAsObservable() {
+
+  }
+}
+```
+
+### Lifecycle
+![犬](images/DialogLifecycle.png)
